49e38d7fa403dbf698b4ab0349301b0a4ef9f3f1
wellmind <daniel.wmr23@gmail.com>
2025-01-31 18:16:59 -0600

    added cloudwatch event

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -57,3 +57,28 @@ resource "aws_lambda_function" "test_lambda" {
     }
   }
 }
+
+# Crear una regla de EventBridge que se ejecute cada día
+resource "aws_cloudwatch_event_rule" "daily_rule" {
+  name        = "daily_event_rule"
+  description = "Regla para ejecutar Lambda cada día"
+  schedule_expression = "rate(1 day)"  # Esto ejecuta la Lambda cada 24 horas
+
+  # Se puede agregar más detalles como filtros si es necesario
+}
+
+# Vincular la función Lambda como destino de la regla de EventBridge
+resource "aws_cloudwatch_event_target" "lambda_target" {
+  rule      = aws_cloudwatch_event_rule.daily_rule.name
+  arn       = aws_lambda_function.test_lambda.arn
+}
+
+# Otorgar permisos a EventBridge para ejecutar Lambda
+resource "aws_lambda_permission" "allow_eventbridge" {
+  statement_id  = "AllowExecutionFromEventBridge"
+  action        = "lambda:InvokeFunction"
+  principal     = "events.amazonaws.com"
+  function_name = aws_lambda_function.test_lambda.function_name
+  source_arn    = aws_cloudwatch_event_rule.daily_rule.arn
+}
+
